bf4d3c1c29af8b2178a93b561e34100ec7079f32
João Victor Gregório da Silva <146145021+joaovictorgregorio@users.noreply.github.com>
2024-03-21 20:44:58 -0300

    Desenvolvido a multiplicação na calculadora.

---
+++ Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Calculator {
+    class Program {
+        static void Main(string[] args) {
+            Multiplicacao();
+        }
+
+        static void Multiplicacao() {
+            // Limpa o console antes da execução.
+            Console.Clear();
+            Console.Write("Digite o primeiro valor: ");
+            float numberOne = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            float numberTwo = float.Parse(Console.ReadLine());
+
+            float multiplication = numberOne * numberTwo;
+            Console.WriteLine("");
+            Console.WriteLine($"O resultado da multiplicação é: {multiplication}");
+            // Segura a finalização do programa em execução. Apenas quando pressionar no teclado Enter ele finaliza.
+            Console.ReadKey();
+        }
+    }
+}
