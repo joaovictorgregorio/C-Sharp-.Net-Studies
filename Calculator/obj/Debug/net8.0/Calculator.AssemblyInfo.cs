3c39ccd1e5526c526ce06a4fa9e19a498461553b
João Victor Gregório da Silva <146145021+joaovictorgregorio@users.noreply.github.com>
2024-03-21 17:52:10 -0300

    Calculadora criada, exercício de codificação.

--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("MeuApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+046adfcb72a5955f2dd345ea0e43b34c08636f85")]
-[assembly: System.Reflection.AssemblyProductAttribute("MeuApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("MeuApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
