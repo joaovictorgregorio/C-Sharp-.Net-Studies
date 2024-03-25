e1dc061fd6ff8792e18f85a17979d2d09a0d23f4
João Victor Gregório da Silva <146145021+joaovictorgregorio@users.noreply.github.com>
2024-03-24 22:30:45 -0300

    Projeto Timer em em desenvolvimento.

---
+++ My-Personal-Projects-With-C#/Timer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Media;
+
+namespace Timer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("INSTRUÇÕES...");
+            Console.WriteLine("--------------------");
+            Thread.Sleep(1000);
+            Console.WriteLine("Segundos = s. Exemplo 10s");
+            Console.WriteLine("Minutos = m. Exemplo 1m");
+            Console.WriteLine("Horas igual = h. Exemplo 1h");
+            Console.WriteLine("--------------------");
+            Console.Write("Quanto tempo você deseja contar? ");
+            Thread.Sleep(1000);
+
+            // Capturar os dados digitado pelo usuário. E transformar os caracteres em minúsculos.
+            string time = Console.ReadLine().ToLower();
+            
+            Console.WriteLine(time);
+        }
+        static void Count(int time)
+        {
+            Console.Clear();
+
+            int currentTime = 1;
+
+            for (int i = 1; currentTime <= time; i++)
+            {
+                Console.Clear();
+                Console.WriteLine(i);
+                Thread.Sleep(300);
+                currentTime++;
+            }
+        }
+    }
+}
