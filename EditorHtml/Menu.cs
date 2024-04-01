0481c22b4bb19071ea15f9aa7f2d87ba3f247cc3
João Victor Gregório da Silva <146145021+joaovictorgregorio@users.noreply.github.com>
2024-04-01 12:52:16 -0300

    Finalizado Editor HTML em aula.

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Channels;
 
 namespace EditorHtml
 {
@@ -7,10 +10,97 @@ namespace EditorHtml
         public static void Show()
         {
             Console.Clear();
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
 
-            
+            DrawScreen();
+            WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
+            HandleMenuOption(option);
+        }
+
+        public static void DrawingScreenLine()
+        {
+            Console.Write("+");
+
+            for (int i = 0; i <= 30; i++)
+            {
+                Console.Write("-");
+                Thread.Sleep(5);
+            }
+
+            Console.Write("+ \n");
+        }
+
+        public static void DrawingScreenColumn()
+        {
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+
+                for (int i = 0; i <= 30; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write("| \n");
+            }
+        }
+
+        public static void DrawScreen()
+        {
+            DrawingScreenLine();
+
+            DrawingScreenColumn();
+
+            DrawingScreenLine();
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(10, 1);
+            Thread.Sleep(200);
+            Console.WriteLine("Editor HTML");
+
+            Console.SetCursorPosition(4, 2);
+            Console.WriteLine("=======================");
+            Thread.Sleep(200);
+
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("Selecione uma opção abaixo: ");
+            Thread.Sleep(200);
+
+            Console.SetCursorPosition(2, 5);
+            Console.WriteLine("1 - Novo arquivo");
+            Thread.Sleep(300);
+
+            Console.SetCursorPosition(2, 6);
+            Console.WriteLine("2 - Abrir");
+            Thread.Sleep(300);
+
+            Console.SetCursorPosition(2, 7);
+            Console.WriteLine("0 - Sair");
+            Thread.Sleep(300);
+
+            Console.SetCursorPosition(2, 8);
+            Console.Write("Opção: ");
+            Thread.Sleep(300);
+        }
+
+        public static void HandleMenuOption(short option)
+        {
+            switch (option)
+            {
+                case 1: Editor.Show(); break;
+                case 2: Console.WriteLine("View"); break;
+                case 0: {
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                }
+                default: Show(); break;
+            }
         }
     }
 }
--- EditorHtml/obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
+++ EditorHtml/obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d280bfeb385a34e5bf31bafb79b2780cdfbed5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca89fb7fdaea394c1dcfe2874399688771c43c4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
