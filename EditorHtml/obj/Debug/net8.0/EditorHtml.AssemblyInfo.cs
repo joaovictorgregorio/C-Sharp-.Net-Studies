ca89fb7fdaea394c1dcfe2874399688771c43c4b
João Victor Gregório da Silva <146145021+joaovictorgregorio@users.noreply.github.com>
2024-03-31 21:54:13 -0300

    Iniciado projeto Editor HTML.

--- EditorHtml/obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
+++ EditorHtml/obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("StudiesMyApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d280bfeb385a34e5bf31bafb79b2780cdfbed5e")]
-[assembly: System.Reflection.AssemblyProductAttribute("StudiesMyApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("StudiesMyApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("EditorHtml")]
+[assembly: System.Reflection.AssemblyTitleAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
