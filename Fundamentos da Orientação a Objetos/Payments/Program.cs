6417ea2f418b050bc8985e643012d0aee9326ee6
Joao Victor Gregorio <joaogregoriodsilva@gmail.com>
2024-04-15 11:47:18 -0300

    Conceitos sobre Classe, Objeto, Encapsulamento, Abstração e herança

---
+++ Fundamentos da Orientação a Objetos/Payments/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography;
+using static System.Console;
+
+Clear();
+
+// Instânciamento da classe PagamentoBoleto. Instância é o ato de transformar a classe em um objeto. 
+var pagamentoBoleto = new PagamentoBoleto();
+pagamentoBoleto.Pagar();
+pagamentoBoleto.Vencimento = DateTime.Now;
+pagamentoBoleto.NumeroBoleto = "1234";
+
+var pagamento = new Pagamento();
+
+var pagamentoCartaoCredito = new PagamentoCartaoCredito();
+pagamentoCartaoCredito.Vencimento = DateTime.Now;
+
+class Pagamento {
+
+    // Propriedades
+    public DateTime Vencimento;
+
+    // Métodos
+    public void Pagar() {
+        ConsultarSaldoDoCartao("123456789");
+    }
+
+    // Irá privar o acesso feito por outras classes, conceito de abstração. Neste caso, funciona apenas na classe Pagamento.
+    private void ConsultarSaldoDoCartao(string numerocartao) {
+        
+    }
+}
+
+// Aplicado conceito de herança. Neste caso, PagamentoBoleto herda de Pagamento.
+class PagamentoBoleto : Pagamento {
+    public string NumeroBoleto;
+}
+
+// Não é possível herança múltipla no C#, apenas Pai e filho, sendo heranças únicas. Sem limite de uso, processo pode ser repetido quantas vezes for necessário.
+class PagamentoCartaoCredito : Pagamento {
+    public string Numero;
+}
