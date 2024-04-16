e36c42582549fbd24ead2d45d601a81231627366
Joao Victor Gregorio <joaogregoriodsilva@gmail.com>
2024-04-16 10:43:52 -0300

    Estudos sobre Polimorfismo, Modificadores de acesso, Tipos complexos e Propriedades seus conceitos de uso

--- Fundamentos da Orientação a Objetos/Payments/Program.cs
+++ Fundamentos da Orientação a Objetos/Payments/Program.cs
@@ -10,18 +10,37 @@ pagamentoBoleto.Vencimento = DateTime.Now;
 pagamentoBoleto.NumeroBoleto = "1234";
 
 var pagamento = new Pagamento();
+pagamento.DataPagamento = DateTime.Now;
 
 var pagamentoCartaoCredito = new PagamentoCartaoCredito();
 pagamentoCartaoCredito.Vencimento = DateTime.Now;
 
+// Os modificadores no C# são: private, protected, internal e public. Se deixar sem nada à frente da classe, será mesma coisa que deixar private. Protected o acesso é permitido apenas para os filhos que herdam do pai. Internal fica disponível dentro do mesmo namespace, muito pouco usado, casos bem específicos. E public fica disponível para uso onde for necessário.
 class Pagamento {
 
     // Propriedades
-    public DateTime Vencimento;
+    public DateTime Vencimento { get; set; }
+    private DateTime _dataPagamento;
+    public DateTime DataPagamento {
+        get { 
+            Console.WriteLine("Lendo o valor");
+            return _dataPagamento.AddDays(1); 
+        }
+        set {
+            Console.WriteLine("Atribuir um valor"); 
+            _dataPagamento = value; 
+        }
+    }
+    
+    public int MyProperty { get; private set; }
 
-    // Métodos
-    public void Pagar() {
-        ConsultarSaldoDoCartao("123456789");
+    // Métodos. Virtual é um modificador que permite, que o método seja sobreescrito. Conceito do polimorfismo.
+    public virtual void Pagar() {
+        
+    }
+
+    public override string ToString() {
+        return Vencimento.ToString("dd/MM/yyyy");
     }
 
     // Irá privar o acesso feito por outras classes, conceito de abstração. Neste caso, funciona apenas na classe Pagamento.
@@ -33,9 +52,22 @@ class Pagamento {
 // Aplicado conceito de herança. Neste caso, PagamentoBoleto herda de Pagamento.
 class PagamentoBoleto : Pagamento {
     public string NumeroBoleto;
+    // Quando é usado Virtual na classe Pagamento que é o pai, e Override na classe PagamentoBoleto que é o filho. O método Pagar() está sendo sobreescrito, dando uma nova função para este mesmo método. Conceito do polimorfismo.
+    public override void Pagar() {
+        // Regra do boleto
+    }
+
+    void Teste() {
+        // Para verificar as propriedades e métodos disponíveis do pai, neste caso Pagamento, usar a palavra BASE.
+    }
 }
 
 // Não é possível herança múltipla no C#, apenas Pai e filho, sendo heranças únicas. Sem limite de uso, processo pode ser repetido quantas vezes for necessário.
 class PagamentoCartaoCredito : Pagamento {
     public string Numero;
+
+    public override void Pagar()
+    {
+        // Regra cartão de crédito
+    }
 }
--- Fundamentos da Orientação a Objetos/Payments/obj/Debug/net8.0/Payments.AssemblyInfo.cs
+++ Fundamentos da Orientação a Objetos/Payments/obj/Debug/net8.0/Payments.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Payments")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05e77109d964f056d77131ec4efb4c3328969005")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6417ea2f418b050bc8985e643012d0aee9326ee6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Payments")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Payments")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
