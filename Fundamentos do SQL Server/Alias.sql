b6910e9c95bd8e9d72fdfa12c7d02816920a5d79
Joao Victor Gregorio <joaogregoriodsilva@gmail.com>
2024-05-20 17:33:36 -0300

    Aplicando mais conceitos do SQL: Alias, Group By, Inner Join, Having, Views e Union

---
+++ Fundamentos do SQL Server/Alias.sql
@@ -0,0 +1,5 @@
+SELECT TOP 100
+    [Id] AS [Codigo], -- Conceito de (ALIAS) apelido para coluna da tabela.
+    [Nome]
+FROM
+    [Curso]
---
+++ Fundamentos do SQL Server/Union.sql
@@ -0,0 +1,13 @@
+SELECT TOP 100
+    [Id],
+    [Nome]
+FROM
+    [Curso]
+
+UNION ALL
+
+    SELECT TOP 100
+        [Id],
+        [Nome]
+    FROM
+        [Categoria]
