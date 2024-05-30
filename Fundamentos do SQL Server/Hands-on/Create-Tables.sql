a044ef33302abd40ceb0acf67964263d788c5a67
Joao Victor Gregorio <joaogregoriodsilva@gmail.com>
2024-05-30 19:11:33 -0300

    Criado todas as tabelas necessárias para o projeto

--- Fundamentos do SQL Server/Hands-on/Create-Tables.sql
+++ Fundamentos do SQL Server/Hands-on/Create-Tables.sql
@@ -88,3 +88,39 @@ CREATE TABLE [Course]
         REFERENCES [Category] ([Id])
 );
 GO
+
+USE [balta]
+CREATE TABLE [CareerItem] 
+(
+    [CourseId] UNIQUEIDENTIFIER NOT NULL,
+    [CareerId] UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Description] TEXT NOT NULL,
+    [Order] TINYINT NOT NULL,
+    
+
+    CONSTRAINT [PK_CareerItem] PRIMARY KEY ([CourseId], [CareerId]),
+    CONSTRAINT [FK_CareerItem_Career_CareerId] FOREIGN KEY ([CareerId])
+        REFERENCES [Career] ([Id]),
+    CONSTRAINT [FK_CareerItem_Course_CourseId] FOREIGN KEY ([CourseId])
+        REFERENCES [Course] ([Id])
+);
+GO
+
+USE [balta]
+CREATE TABLE [StudentCourse] 
+(
+    [CourseId] UNIQUEIDENTIFIER NOT NULL,
+    [StudentId] UNIQUEIDENTIFIER NOT NULL,
+    [Progress]  TINYINT NOT NULL,
+    [Favorite] BIT NOT NULL,
+    [StartDate] DATETIME NOT NULL,
+    [LastUpdateDate] DATETIME NULL DEFAULT(DATEADD(HOUR, -3, GETUTCDATE()))
+
+    CONSTRAINT [PK_StudentCourse] PRIMARY KEY ([CourseId], [StudentId]),
+    CONSTRAINT [FK_StudentCourse_Course_CourseId] FOREIGN KEY ([CourseId])
+        REFERENCES [Course] ([Id]),
+    CONSTRAINT [FK_StudentCourse_Student_StudentId] FOREIGN KEY ([StudentId])
+        REFERENCES [Student] ([Id])
+);
+GO
